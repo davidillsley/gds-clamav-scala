e30d15d6ac749acb25e0e310231ac56f16f35274
David Illsley <david@illsley.org>
2013-07-21 14:48:42 +0100

    Update clamav-travis.sh

--- clamav-travis.sh
+++ clamav-travis.sh
@@ -2,6 +2,7 @@
 apt-get update -qq
 apt-get install -qq clamav-daemon
 freshclam
+ls -al /etc/clamav/clamd.conf
 echo TCPSocket 3310 >> /etc/clamav/clamd.conf
 /etc/init.d/clamav-daemon start
 cat /etc/clamav/clamd.conf
